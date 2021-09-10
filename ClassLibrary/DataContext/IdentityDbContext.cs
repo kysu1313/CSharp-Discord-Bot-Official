4d81ae88ae49d4d3db45f26a0314c5b54ea85533
Kyle Supple <38538913+kysu1313@users.noreply.github.com>
2021-09-09 21:30:54 -0400

    Before custom user type

---
+++ ClassLibrary/DataContext/IdentityDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace ClassLibrary.DataContext
+{
+    // public class IdentityUserContext<UserModel>
+    //     : IdentityUserContext<UserModel, string>
+    //     where UserModel : IdentityUser
+    // {
+    //     
+    // }
+}
--- ClassLibrary/Models/ContextModels/UserModel.cs
+++ ClassLibrary/Models/ContextModels/UserModel.cs
@@ -5,10 +5,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace ClassLibrary.Models.ContextModels
 {
-    public class UserModel
+    public class UserModel : IdentityUser
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
