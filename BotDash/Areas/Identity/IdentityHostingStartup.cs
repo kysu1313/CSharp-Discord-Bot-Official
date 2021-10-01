a8ca87eea24a0c04c4c4cef3372703a6f5e2849e
Kyle Supple <38538913+kysu1313@users.noreply.github.com>
2021-09-30 22:10:44 -0400

    scaffolded register page

---
+++ BotDash/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿// using System;
+// using ClassLibrary.Data;
+// using ClassLibrary.Models.ContextModels;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.Identity;
+// using Microsoft.AspNetCore.Identity.UI;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// [assembly: HostingStartup(typeof(BotDash.Areas.Identity.IdentityHostingStartup))]
+// namespace BotDash.Areas.Identity
+// {
+//     public class IdentityHostingStartup : IHostingStartup
+//     {
+//         public void Configure(IWebHostBuilder builder)
+//         {
+//             builder.ConfigureServices((context, services) => {
+//                 services.AddDbContext<ApplicationDbContext>(options =>
+//                     options.UseSqlServer(
+//                         context.Configuration.GetConnectionString("DefaultDb")));
+//
+//                 services.AddDefaultIdentity<UserModel>(options => options.SignIn.RequireConfirmedAccount = true)
+//                     .AddEntityFrameworkStores<ApplicationDbContext>();
+//             });
+//         }
+//     }
+// }
